34525a809bc117fa43a4870d43157a883c2d8b02
cocahonka <cocahonka@gmail.com>
2023-04-08 00:24:17 +0300

    fix newStack saltBlock

--- src/main/java/com/cocahonka/saltomaru/salt_block/SaltBlock.kt
+++ src/main/java/com/cocahonka/saltomaru/salt_block/SaltBlock.kt
@@ -6,7 +6,6 @@ import org.bukkit.Material
 import org.bukkit.Sound
 import org.bukkit.block.Block
 import org.bukkit.block.BlockFace
-import org.bukkit.block.data.BlockData
 import org.bukkit.block.data.Directional
 import org.bukkit.inventory.ItemStack
 
@@ -37,7 +36,15 @@ class SaltBlock {
         }
 
         fun getNewItemStack(n: Int = 1): ItemStack {
-            return ItemStack(Material.WHITE_GLAZED_TERRACOTTA, n)
+            val saltBlock = ItemStack(Material.WHITE_GLAZED_TERRACOTTA, n)
+            val meta = saltBlock.itemMeta
+
+            meta.lore(listOf(loreComponent))
+            meta.displayName(nameComponent)
+
+            saltBlock.itemMeta = meta
+
+            return saltBlock
         }
 
         fun createBlockData() : Directional {
