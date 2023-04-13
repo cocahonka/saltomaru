ea211baa1aaac3fb42805d027043f4bbf385aa49
cocahonka <cocahonka@gmail.com>
2023-04-13 17:06:25 +0300

    fix bug (StackOverflow) on clearMatrix

--- src/main/java/com/cocahonka/saltomaru/utils/SaltomaruCraftingUtils.kt
+++ src/main/java/com/cocahonka/saltomaru/utils/SaltomaruCraftingUtils.kt
@@ -1,5 +1,6 @@
 package com.cocahonka.saltomaru.utils
 
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.inventory.CraftingInventory
 import org.bukkit.inventory.ItemStack
@@ -8,32 +9,39 @@ abstract class SaltomaruCraftingUtils {
     companion object {
         fun isValidMatrix(
             matrix: Array<out ItemStack?>,
-            itemValidator: (ItemStack) -> Boolean
+            itemValidator: (ItemStack) -> Boolean,
         ): Boolean {
             for (item in matrix) {
-                if(!(item == null || itemValidator(item))){
+                if (!(item == null || itemValidator(item))) {
                     return false
                 }
             }
             return true
         }
 
-        fun retrievePlayerCraft(inventory: CraftingInventory, player: Player){
-            retrieveMatrix(inventory, player)
+        fun retrievePlayerCraft(inventory: CraftingInventory, player: Player) {
+            retrieveMatrix(inventory.matrix, player)
             clearMatrix(inventory)
         }
 
-        private fun retrieveMatrix(inventory: CraftingInventory, player: Player){
-            for (item in inventory.matrix) {
-                if (item != null) {
-                    player.inventory.addItem(item)
+        private fun retrieveMatrix(matrix: Array<out ItemStack?>, player: Player) {
+            for (item in matrix) {
+                if(item != null) {
+                    val remaining = player.inventory.addItem(item)
+                    if (remaining.isNotEmpty()) {
+                        for (leftover in remaining.values) {
+                            player.world.dropItemNaturally(player.location, leftover)
+                        }
+                    }
                 }
             }
         }
 
         private fun clearMatrix(inventory: CraftingInventory) {
-            for (i in 0 until inventory.size) {
-                inventory.setItem(i, null)
+            for (item in inventory.matrix) {
+                if(item != null) {
+                    inventory.remove(item.type)
+                }
             }
         }
     }
