380d3a215ca828104c4f78df58fed4f4df938540
cocahonka <cocahonka@gmail.com>
2023-04-13 18:59:56 +0300

    doc base package & fix isValidItem

--- src/main/java/com/cocahonka/saltomaru/base/SaltomaruBlockEvent.kt
+++ src/main/java/com/cocahonka/saltomaru/base/SaltomaruBlockEvent.kt
@@ -5,14 +5,29 @@ import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.entity.EntityExplodeEvent
 
+/**
+ * Интерфейс событий для кастомных блоков
+ */
 interface SaltomaruBlockEvent : SaltomaruEvent {
 
+    /**
+     * Функция обрабатывающая событие ломание блока
+     * @param event событие ломания блока
+     */
     @EventHandler
     fun onBlockBreak(event: BlockBreakEvent)
 
+    /**
+     * Функция обрабатывающая событие установки блока в мире
+     * @param event событие установки блока
+     */
     @EventHandler
     fun onBlockPlace(event: BlockPlaceEvent)
 
+    /**
+     * Функция обрабатывающая событие взрыва энтини (нужно для обработки взрыва блока)
+     * @param event событие взырва энтини
+     */
     @EventHandler
     fun onEntityExplode(event: EntityExplodeEvent)
 }
--- src/main/java/com/cocahonka/saltomaru/base/SaltomaruItemCraftable.kt
+++ src/main/java/com/cocahonka/saltomaru/base/SaltomaruItemCraftable.kt
@@ -4,11 +4,25 @@ import org.bukkit.NamespacedKey
 import org.bukkit.event.EventHandler
 import org.bukkit.event.inventory.PrepareItemCraftEvent
 
+/**
+ * Интерфейс для крафта кастомных предметов
+ * @property recipeKey основной ключ рецепта (для дальнейшей регистрации крафта)
+ */
 interface SaltomaruItemCraftable : SaltomaruItem, SaltomaruEvent {
     val recipeKey: NamespacedKey
 
+    /**
+     * Функция регистрации рецепта
+     * @return возвращает true если предмет успешно зарегистрирован
+     */
     fun registerItemRecipe(): Boolean
 
+    /**
+     * Событие выкладывания предметов для крафта
+     *
+     * Нужно для валидации использованных предметов в крафте
+     * @param event событие выкладывания предметов для крафта
+     */
     @EventHandler
     fun onPrepareItemCraft(event: PrepareItemCraftEvent)
 }
