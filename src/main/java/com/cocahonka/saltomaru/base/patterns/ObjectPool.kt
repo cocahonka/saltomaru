9f0667882e47503b1b582580a27bcf0bf31fc2bb
cocahonka <cocahonka@gmail.com>
2023-04-21 19:58:18 +0300

    refactor object pool to update reuseble objects

--- src/main/java/com/cocahonka/saltomaru/base/patterns/ObjectPool.kt
+++ src/main/java/com/cocahonka/saltomaru/base/patterns/ObjectPool.kt
@@ -3,27 +3,33 @@ package com.cocahonka.saltomaru.base.patterns
 import java.util.concurrent.ConcurrentLinkedQueue
 
 /**
- * Универсальный, потокобезопасный класс пула объектов.
+ * Универсальный класс пула объектов.
  *
  * @param T тип объектов, управляемых пулом.
+ * @param A тип аргумента, необходимого для создания и обновления объекта.
  * @param factory функция, которая создает новый экземпляр типа [T].
+ * @param update функция, которая обновляет существующий экземпляр типа [T] с использованием аргумента типа [A].
  *
  * @property factory функция-фабрика, используемая для создания новых экземпляров типа [T].
+ * @property update функция, используемая для обновления существующих экземпляров типа [T].
  * @property pool очередь, которая хранит доступные экземпляры объектов типа [T].
+ *
  */
-class ObjectPool<T>(private val factory: () -> T) {
+class ObjectPool<T, A>(private val factory: (A) -> T, private val update: (T, A) -> Unit) {
     private val pool = ConcurrentLinkedQueue<T>()
 
     /**
-     * [acquire] извлекает доступный экземпляр объекта из пула или создает новый, если пул пуст.
-     * @return экземпляр типа [T]
+     *  [acquire] извлекает доступный экземпляр объекта из пула, обновляет его с использованием
+     *  аргумента типа [A] и возвращает его.
      */
-    fun acquire(): T {
-        return pool.poll() ?: factory()
+    fun acquire(arg: A): T {
+        val item = pool.poll() ?: factory(arg)
+        update(item, arg)
+        return item
     }
 
     /**
-     * [release] возвращает экземпляр объекта типа [T] обратно в пул.
+     * [release] возвращает экземпляр объекта обратно в пул.
      */
     fun release(item: T) {
         pool.offer(item)
