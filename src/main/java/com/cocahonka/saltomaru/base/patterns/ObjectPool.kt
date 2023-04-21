8a71cba9ec5bd32d232857fcd8b5c249da1afc10
cocahonka <cocahonka@gmail.com>
2023-04-21 19:09:13 +0300

    add ObjectPool class (pattern object pool)

--- src/main/java/com/cocahonka/saltomaru/base/patterns/ObjectPool.kt
+++ src/main/java/com/cocahonka/saltomaru/base/patterns/ObjectPool.kt
@@ -1,4 +1,27 @@
 package com.cocahonka.saltomaru.base.patterns
 
-class ObjectPool {
+import java.util.concurrent.ConcurrentLinkedQueue
+
+/**
+ * Универсальный, потокобезопасный класс пула объектов.
+ *
+ * @param T тип объектов, управляемых пулом.
+ * @param factory функция, которая создает новый экземпляр типа [T].
+ *
+ * @property factory функция-фабрика, используемая для создания новых экземпляров типа [T].
+ * @property pool очередь, которая хранит доступные экземпляры объектов типа [T].
+ *
+ * @function acquire извлекает доступный экземпляр объекта из пула или создает новый, если пул пуст.
+ * @function release возвращает экземпляр объекта обратно в пул.
+ */
+class ObjectPool<T>(private val factory: () -> T) {
+    private val pool = ConcurrentLinkedQueue<T>()
+
+    fun acquire(): T {
+        return pool.poll() ?: factory()
+    }
+
+    fun release(item: T) {
+        pool.offer(item)
+    }
 }
